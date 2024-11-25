4b0dc27feb37af1d1aafdd07812e39a0bf72acc7
quanho1234 <quanho7905@gmail.com>
2024-11-25 21:17:37 +0700

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,11 @@
+print("Sinh vien: Ho Anh Quan")
+print("Ma so SV : 235752021610088")
+print("#######################################")
+################################################
+
+n1=int(input("enter n1 value: "))
+n2=int(input("enter n2 value: "))
+if n1>n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
